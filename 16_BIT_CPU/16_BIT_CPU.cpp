152e4c54f3563df5d6ea0324cecc5f86bb9ac951
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-04-29 22:37:39 -0700

    Delete 16_BIT_CPU.cpp

--- 16_BIT_CPU/16_BIT_CPU.cpp
+++
@@ -1,9 +0,0 @@
-// 16_BIT_CPU.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
